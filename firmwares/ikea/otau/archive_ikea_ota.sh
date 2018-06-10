5d5307583b8a64049946fa15d0530f0e674fbc1d
Olliver Schinagl <oliver@schinagl.nl>
2018-06-10 15:35:03 +0200

    firmwares: Add cron script to have archive.org index the json
    This is mostly for archival purposes as I am running the once a week
    via cron.weekly.
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

---
+++ firmwares/ikea/otau/archive_ikea_ota.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl "https://web.archive.org/save/http://fw.ota.homesmart.ikea.net/feed/version_info.json"
+curl "https://web.archive.org/save/http://fw.test.ota.homesmart.ikea.net/feed/version_info.json"
